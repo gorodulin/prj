4094c265a5698b459c538abfe31bec9ec4f86382
Vladimir Gorodulin <ru.hostmaster@gmail.com>
2026-04-06 15:28:46 +0100

    Update packaging for v0.4.0 release

--- packaging/homebrew/prj.rb
+++ packaging/homebrew/prj.rb
@@ -1,8 +1,8 @@
 class Prj < Formula
   desc "Projector: project folder, metadata, and link manager"
   homepage "https://github.com/gorodulin/prj"
-  url "https://github.com/gorodulin/prj/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "fd60322a643dedfc2365045218958c84ee76ce548d3c6aea8ae3ba3e2393ca52"
+  url "https://github.com/gorodulin/prj/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "bda460d2dd419a1df5100677d89d56f0781355c626ff25f6b50f02b3195a859e"
   license "Apache-2.0"
 
   depends_on "go" => :build
